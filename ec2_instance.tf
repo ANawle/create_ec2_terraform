f84553c9ca805066f664bb889a77580f8317673d
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 15:28:56 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "my_key" {
-  key_name   = "avi-key"  # Name of the key pair
-  public_key = file("/c/Users/js/.ssh/id_rsa.pub")  # Path to your public key
+  key_name   = "avi-key"
+  public_key = file("C:/Users/js/.ssh/id_rsa.pub")
 }
+
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
