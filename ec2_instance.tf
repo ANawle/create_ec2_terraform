797c75382779bfbd52a005f237bfa1f94ea6d15e
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 14:08:28 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Adjust your region
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "my_instance1" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
 
