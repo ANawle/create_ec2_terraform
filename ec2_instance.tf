a481cc7a8db44487b3e080da92333513625b0c33
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 12:14:22 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,18 +1,14 @@
-provider "aws" {
-  region = "ap-south-1"  # Adjust your region
-}
- 
-resource "aws_instance" "my_instance1" {
-  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
-  instance_type = "t2.micro"
- 
-  tags = {
-    Name = "Avi_instance"
- provider "aws" {
+  provider "aws" {
   region     = "ap-south-1"
   access_key = "AKIA6ODU37ZLXUF2YMDN"
   secret_key = "WEqmYaJb/kab0EZfYwJqE+xspFau+XK0s7IteZQ6"
 }
+
+resource "aws_instance" "my_instance1" {
+  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Avi_instance"
   }
 }
- 
