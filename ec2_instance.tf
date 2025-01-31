0de8d6460f4b29e29ba10fe9638e98484268476a
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 12:09:42 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -8,6 +8,11 @@ resource "aws_instance" "my_instance1" {
  
   tags = {
     Name = "Avi_instance"
+ provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA6ODU37ZLXUF2YMDN"
+  secret_key = "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"
+}
   }
 }
  
