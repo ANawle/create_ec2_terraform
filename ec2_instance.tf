3589fdbbbeb8355343f80328f6ec077c5e9924a9
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 14:08:07 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c614dee691cbbf37"  # Replace with your preferred AMI
+  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
 
   tags = {
