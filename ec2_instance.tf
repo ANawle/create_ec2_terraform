14b71c965c67e86e75be57c4f6f7ac9cd297729e
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 15:47:24 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,18 +2,11 @@ provider "aws" {
   region = "ap-south-1"  # Adjust your region
 }
 
-resource "aws_key_pair" "my_key" {
-  key_name   = "avi-key"
-  public_key = file("C:/Users/js/.ssh/id_rsa.pub")
-}
-
-
 resource "aws_instance" "my_instance" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name  # Attach the key pair
 
   tags = {
-    Name = "Avinash"
+    Name = "Avi"
   }
 }
