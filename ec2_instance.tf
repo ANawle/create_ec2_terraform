2a8bd778208d3da313b716eedf1eafe1a622de69
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 11:56:24 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,72 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIA6ODU37ZLXUF2YMDN"
-  secret_key = "WEqmYaJb/kab0EZfYwJqE+xspFau+XK0s7IteZQ6"
+  region = "ap-south-1"  # Adjust your region
 }
-
-// To Generate Private Key
-resource "tls_private_key" "rsa_4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-variable "key_name" {
-  description = "Name of the SSH key pair"
-}
-
-// Create Key Pair for Connecting EC2 via SSH
-resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
-
-// Save PEM file locally
-resource "local_file" "private_key" {
-  content  = tls_private_key.rsa_4096.private_key_pem
-  filename = var.key_name
-}
-
-# Create a security group
-resource "aws_security_group" "sg_ec2" {
-  name        = "sg_ec2"
-  description = "Security group for EC2"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "auto_create_ec2" {
-  ami                    = "ami-00bb6a80f01f03502"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
-
+ 
+resource "aws_instance" "my_instance1" {
+  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
+  instance_type = "t2.micro"
+ 
   tags = {
-    Name = "auto_create_ec2"
-  }
-
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp2"
+    Name = "Avi_instance"
   }
 }
+ 
