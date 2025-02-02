86cb92895832b3a2c5dacfceed075bfd863a7635
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 13:54:12 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -8,11 +8,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "avibkt"  # Make sure this name is globally unique
   acl    = "public-read"  # Adjust ACL as needed (private, public-read, etc.)
 
-  # Optional: Enable versioning
-  versioning {
-    enabled = true
-  }
-
   # Tags for the bucket
   tags = {
     Name        = "MyS3Bucket"
