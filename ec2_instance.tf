32b88ce24012f6b44b266762b2b85624e623075e
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 14:00:03 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -6,16 +6,11 @@ provider "aws" {
 # S3 Bucket Resource
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "avibkt"  # Make sure this name is globally unique
-  aws_s3_bucket_acl = "public-read"  # Adjust ACL as needed (private, public-read, etc.)
+  
 
   # Tags for the bucket
   tags = {
     Name        = "MyS3Bucket"
     Environment = "dev"
   }
-}
-
-# Optional: Output the bucket name
-output "s3_bucket_name" {
-  value = aws_s3_bucket.my_bucket.bucket
 }
