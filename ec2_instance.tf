58318fb4d0c9e75b6f3cdd95ba1faf4e279dde11
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 11:48:59 +0530

    ec2_with_key_pair

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-00bb6a80f01f03502"
-# Replace with your preferred AMI
+  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
 
+  key_name      = "Spacelift_key"  # Use your existing key pair name here 
 
   tags = {
     Name = "Avinash"
