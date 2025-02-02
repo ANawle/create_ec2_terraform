85215fe0ece736672b183b40bde7dc96fcdc91c1
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 10:58:04 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,22 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
+  ami           = "ami-00bb6a80f01f03502"
+  ami           = "ami-00bb6a80f01f03502"
+# Replace with your preferred AMI
+  instance_type = "t2.micro"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Avi"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "null_resource" "terminate_instance" {
-  depends_on = [aws_instance.my_instance]
-
-  provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.my_instance.id} --region ap-south-1"
+    Name = "Avinash"
   }
 }
