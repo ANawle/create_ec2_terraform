2b8c6ac70ff2eb26e5b68dd40ade85dffa51619f
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 09:45:19 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -5,9 +5,17 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
-  
 
   tags = {
     Name = "Avi"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # Provisioner to stop the instance after creation
+  provisioner "local-exec" {
+    command = "aws ec2 stop-instances --instance-ids ${self.id} --region ${var.region}"
   }
 }
