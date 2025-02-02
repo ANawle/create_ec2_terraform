ccc5f63d6502cce4019f5430b52012eb997ff4c7
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-02 13:38:22 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,44 +2,12 @@ provider "aws" {
   region = "ap-south-1"  # Adjust your region
 }
 
-# Define the Security Group with SSH (22) and HTTP (80) rules
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "Allow SSH and HTTP access"
-
-  # Inbound rules for SSH and HTTP
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from any IP address
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from any IP address
-  }
-
-  # Outbound rule allowing all traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All traffic
-    cidr_blocks = ["0.0.0.0/0"]  # Allow outbound traffic to any destination
-  }
-}
-
-# Create the EC2 instance with the Security Group
 resource "aws_instance" "my_instance" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with your preferred AMI
   instance_type = "t2.micro"
-  key_name      = "Spacelift_key"  # Use your existing key pair name here
-  security_groups = [aws_security_group.my_security_group.name]  # Associate the security group
 
   tags = {
-    Name = "Avinash"
+    Name = "Avi"
   }
 
   lifecycle {
@@ -47,3 +15,10 @@ resource "aws_instance" "my_instance" {
   }
 }
 
+resource "null_resource" "terminate_instance" {
+  depends_on = [aws_instance.my_instance]
+
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.my_instance.id} --region ap-south-1"
+  }
+}
