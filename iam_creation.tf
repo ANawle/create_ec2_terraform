5be39cc5b34b51fd223a9ae25f022124cb7fedc8
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-03 18:36:59 +0530

    create vpc

--- iam_creation.tf
+++ iam_creation.tf
@@ -1,16 +1,54 @@
-# Provider Configuration
-provider "aws" {
-  region = "ap-south-1"  # Replace with your region
+provider aws {
+  region = "ap-south-1"
 }
-	
-# S3 Bucket Resource with force_destroy enabled for deletion
-resource "aws_s3_bucket" "my_bucket" {
-  bucket         = "avinlebkt"  # Make sure this name is globally unique
-  force_destroy  = true      # Allows deletion even if the bucket contains objects
-
-  # Tags for the bucket
+#Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name        = "MyS3Bucket"
-    Environment = "dev"
+    Name = "my_vpc"
   }
+}
+
+#Private subnet
+resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  availability_zone       = "ap-south-1b"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#Public subnet
+resource "aws_subnet" "public-subnet" {
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+#Internet gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+#Routing table
+resource "aws_route_table" "my-rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+}
+
+resource "aws_route_table_association" "public-sub" {
+  route_table_id = aws_route_table.my-rt.id
+  subnet_id      = aws_subnet.public-subnet.id
 }
